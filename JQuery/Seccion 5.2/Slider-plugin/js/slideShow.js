fbf29aa076929471578dfff0167e6135048fd075
nc-impacto <ncaffa@webimpacto.es>
2020-05-11 11:05:40 +0200

    Plugin parte 2

--- JQuery/Seccion 5.2/Slider-plugin/js/slideShow.js
+++ JQuery/Seccion 5.2/Slider-plugin/js/slideShow.js
@@ -1,39 +1,88 @@
 (function(){
 
-	var actual = 0;
-	var ancho  = 600;
 
-	var $slideshow = $(".slideShow ul");
-	var slides  = $slideshow.find("li").length;
-	var $puntos=$(".slidesShowButtons");
+	$.slideShow = function( opciones ){
 
-	$puntos.find("div").eq(0).css({
-		backgroundColor:"#58167d"
-	});
+			opciones = $.extend({
+					divDestino: ".slideShow",
+					intervalor:1500,
+					ancho: 600,
+					velocidad: 1200,
+					slides: [],
+					color: "#ba007c"
+			},opciones);
 
-	var intervalo = setInterval(function(){
-		mover("sig");
-	}, 1500 );
+
+			if(opciones.slides.length == 0){
+				alert("los slides son necesarios.");
+				return;
+			}
+
+			opciones.velocidad= opciones.velocidad/1000;
+
+			var actual = 0;
+			var ancho  = opciones.ancho;
+
+			var slides  = opciones.slides.length;
+
+			// creacion del slideshow
+
+				var contenido="";
+				contenido += "<ul>"
+					for(var i =0;i<opciones.slides.length;i++){
+
+						contenido += '<li><img src="'+opciones.slides[i]+'"></li>';
+
+					}
+				contenido += "</ul>";
+
+				$( opciones.divDestino ).append( contenido );
+
+				var $slideshow = $ (opciones.divDestino + " ul");
+				
+
+			//creacion de los botones
+				contenido = "";
+
+				contenido += '<div class="slidesShowButtons">';
+				
+				for(var i =0;i<opciones.slides.length;i++){
+
+					contenido += '<div data-idx="'+i+'" class="slideButton"></div>';
+
+				}
+
+				contenido += "</div>";
+
+				$(opciones.divDestino).append(contenido);
+
+
+
+			var $puntos=$(".slidesShowButtons");
+
+			$puntos.find("div").eq(0).css({
+				backgroundColor: opciones.color
+			});
+
+		var intervalo = setInterval(function(){
+				mover("sig");
+		}, opciones.intervalo );
 
 
 	function mover( dir){
 
-		( dir === "sig" ) ? actual-- : actual++;
+			( dir === "sig" ) ? actual-- : actual++;
 
-		if( actual > 0 ){
+			if( actual > 0 ){
 
-			actual = ( slides - 1 ) * -1;
+				actual = ( slides - 1 ) * -1;
 
-		}else if( actual <= ( slides * -1 ) ){
-			actual = 0;
+			}else if( actual <= ( slides * -1 ) ){
+				actual = 0;
 		}
 
-		mover_por_punto(actual);
-
-	
-
-
-	}
+			mover_por_punto(actual);
+		}
 
 	function mover_por_punto(actual,click ){
 		
@@ -46,8 +95,8 @@
 		var $demasPuntos = $puntos.find("div").not($puntoActual);
 
 		var tl = new TimelineMax();
-		tl.to( $slideshow, 1.2, { marginLeft: margen, ease: Elastic.easeOut.config(1, 0.75) } )
-			.to($puntoActual, 0.5,{backgroundColor:"#58167d"},"-=1.2")
+		tl.to( $slideshow, opciones.velocidad, { marginLeft: margen, ease: Elastic.easeOut.config(1, 0.75) } )
+			.to($puntoActual, 0.5,{backgroundColor: opciones.color },"-=1.2")
 			.to($demasPuntos,0.5,{backgroundColor:"#a1a1a1"},"-=1.2");
 
 		// $slideshow.animate({
@@ -71,10 +120,12 @@
 
 	$(".botSlide").on("click",function(){
 
-		var dir = $(this).data("mov");
-		mover( dir, true );
+			var dir = $(this).data("mov");
+			mover( dir, true );
 
-	});
+		});
 
+	}
+	
 
 })();
